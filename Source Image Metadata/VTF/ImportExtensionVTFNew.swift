4a6b2a4507ad7b00e8f495221eaac8b3b538f520
C.W. Betts <computers57@hotmail.com>
2025-03-25 02:43:55 -0600

    Pokes.

--- Source Image Metadata/VTF/ImportExtensionVTFNew.swift
+++ Source Image Metadata/VTF/ImportExtensionVTFNew.swift
@@ -55,7 +55,7 @@ public class ImportExtensionVTFNew: CSImportExtension {
 		}
 		
 		let isEnvironmentMap: Bool = file.GetFaceCount() > 1
-		let hasAlphaChannel = VTFImageFlag(rawValue: file.GetFlags()).contains([.TEXTUREFLAGS_ONEBITALPHA, .TEXTUREFLAGS_EIGHTBITALPHA])
+		let hasAlphaChannel = VTFImageFlag(rawValue: file.GetFlags()).contains([.TEXTUREFLAGS_ONEBITALPHA]) || VTFImageFlag(rawValue: file.GetFlags()).contains([.TEXTUREFLAGS_EIGHTBITALPHA])
 		let hasMipmaps: Bool = file.GetMipmapCount() > 1
 		let isAnimated: Bool = file.GetFrameCount() > 1
 		var compression: String? = nil
